95a7679b43c6e239773017848ab0159c1c3a13f7
Sarubi Thillainathan <sarubi@tony-2.coli.uni-saarland.de>
2024-04-26 15:42:48 +0200

    update

---
+++ scripts/test_maxdepth_above_6_val_on_different_ratios_evaluate.py
@@ -0,0 +1,64 @@
+# -- fix path --
+from pathlib import Path
+import sys
+sys.path.append(str(Path(__file__).resolve().parent.parent))
+# -- end fix path --
+
+from source.evaluate import simplify_file, evaluate_all_metrics
+
+features_kwargs = {
+    'DependencyTreeDepthRatioFeature': {'target_ratio': -1},
+    'DependencyTreeLengthRatioFeature': {'target_ratio': -1},
+    'DifficultWordsRatioFeature': {'target_ratio': -1},
+    'WordCountRatioFeature': {'target_ratio': -1}
+}
+
+experiment_name="exp_1713803608466002_epoch_5_model_eval_loss"
+dataset_base_dir_name=Path("/nethome/sarubi/A8/ptm_access_based_ft/TS_T5_myedit/LLM_based_control_rewrite/experiments/data_filtered_regression_model/T5_ft/f4_maxdepdepth_maxdeplength_diffwords_wc/test_maxdepdepth_above_6")
+
+datasets_to_evaluate = {
+    "gold_ratios" : "processed_data/wikilarge.test/gold_ratios",
+    "fixed_ratios" : "processed_data/wikilarge.test/0_fr_ratios"
+}
+
+for dataset_name_key, filepath in datasets_to_evaluate.items():
+    output_dir = dataset_base_dir_name / f'{dataset_name_key}-filtered_wiki.maxdepdepth_above_6.v2_wo_line_46.valid-t5ft-{experiment_name}/maxdepdepth_-1_maxdeplength_-1_diffwordscount_-1_avgwordcount_-1_length_-1_leven_-1'
+    output_dir.mkdir(parents=True, exist_ok=True)
+    print(f"Evaluate dataset: {filepath}, output will be saved to: {output_dir}")
+
+    # Define the source file paths
+    complex_source_filepath = dataset_base_dir_name / f'{filepath}/test.src'
+    simple_target_filepath = dataset_base_dir_name / f'{filepath}/test.tgt'
+    complex_processed_filepath = dataset_base_dir_name / f'{filepath}/test.src_processed'
+
+    # List of source files and their corresponding output file names
+    files_to_copy = [
+        (complex_source_filepath, output_dir / 'input.txt'),
+        (simple_target_filepath, output_dir / 'gold_ref.txt'),
+        (complex_processed_filepath, output_dir / 'input.processed.txt')
+    ]
+
+    # Function to copy the first 200 lines from each file
+    def copy_first_200_lines(source_path, dest_path):
+        with open(source_path, 'r', encoding='utf-8') as source_file:
+            with open(dest_path, 'w', encoding='utf-8') as dest_file:
+                for _ in range(200):
+                    line = source_file.readline()
+                    if not line:
+                        break
+                    dest_file.write(line)
+
+    # Apply the function to each file pair
+    for source, dest in files_to_copy:
+        copy_first_200_lines(source, dest)
+
+    output_filepath = output_dir / f'output.txt'
+
+    # model_dirname=None will use the last trained model from the folder experiments
+    repo_dir = Path(__file__).resolve().parent.parent
+    model_dirname = repo_dir / "experiments" / experiment_name
+    simplify_file(output_dir / 'input.processed.txt', output_filepath, features_kwargs, model_dirname=model_dirname)
+
+    print(evaluate_all_metrics(output_dir / 'input.txt', output_filepath, [output_dir / 'gold_ref.txt']))
+
+# CUDA_VISIBLE_DEVICES=0 python scripts/test_maxdepth_above_6_val_on_different_ratios_evaluate.py > experiments/exp_1713803608466002_epoch_5_model_eval_loss/logs_TEST_maxdepth_above_6_evaluate_across_all_ratios
--- setup_run.sh
+++ setup_run.sh
@@ -27,3 +27,39 @@ python -m spacy download en_core_web_md
 
 CUDA_VISIBLE_DEVICES=0,1,2,3 python scripts/train.py
 CUDA_VISIBLE_DEVICES=3 python scripts/train.py
+
+#CUDA_VISIBLE_DEVICES=3 python scripts/train.py > experiments/logs_epoch_5_batch16_with_gold_ratio_sari_eval
+#CUDA_VISIBLE_DEVICES=1 python scripts/train.py > experiments/logs_epoch_1_batch16_with_gold_ratio_sari_eval
+#
+#CUDA_VISIBLE_DEVICES=1 python scripts/train.py > experiments/logs_epoch_1_batch16_with_gold_ratio_sari_eval_run_2
+#
+#CUDA_VISIBLE_DEVICES=1 python scripts/generate.py > experiments/exp_1713749698488586/logs_generate
+#
+#
+## val loss default entropy
+#CUDA_VISIBLE_DEVICES=1 python scripts/train.py > experiments/logs_epoch_1_batch16_with_model_eval_loss
+#
+#CUDA_VISIBLE_DEVICES=3 python scripts/train.py > experiments/logs_epoch_5_batch16_with_model_eval_loss
+#
+#
+#CUDA_VISIBLE_DEVICES=1 python scripts/generate.py > experiments/exp_1713803110061108/logs_generate_valid
+#CUDA_VISIBLE_DEVICES=1 python scripts/generate.py > experiments/exp_1713803110061108/logs_generate_test_maxdepth_above_6
+#
+#
+#
+## sari loss default fixed ratio 0.8
+#CUDA_VISIBLE_DEVICES=0 python scripts/train.py > experiments/logs_epoch_1_batch16_with_sari_loss_fr_0.8
+#
+#CUDA_VISIBLE_DEVICES=3 python scripts/train.py > experiments/logs_epoch_1_correct_batch16_with_sari_loss_fr_0.8_gpu3
+#
+#CUDA_VISIBLE_DEVICES=4 python scripts/train.py > experiments/logs_debug_default_sari_loss_with_fr_0.8
+
+
+
+# train v3 - model eval loss
+CUDA_VISIBLE_DEVICES=3 python scripts/train.py > experiments/logs_epoch_5_batch16_with_train_v3_model_eval_loss
+
+CUDA_VISIBLE_DEVICES=1 python scripts/train.py > experiments/logs_epoch_1_batch16_with_train_v3_model_eval_loss
+
+#CUDA_VISIBLE_DEVICES=0 python scripts/train.py > experiments/logs_debug_update_20_with_train_v3_model_eval_loss
+
